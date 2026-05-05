d332badcaa217058050a58429d6001e946cebd9f
maariiiaa8 <mariabuznav@gmail.com>
2026-05-05 12:51:43 +0200

    Mejora del terminal del lado del servidor terminada.

--- src/main/java/servidor/Servidor.java
+++ src/main/java/servidor/Servidor.java
@@ -1,5 +1,7 @@
 package servidor;
 
+import util.Terminal;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -10,46 +12,50 @@ import java.util.Scanner;
 public class Servidor {
     public static void main(String[] args) throws Exception {
         int PUERTO = 5000;
+
+        Terminal.cabecera("SERVER " + PUERTO);
+        Terminal.cmd("INICIALIZANDO PROTOCOLO DE COMUNICACIÓN SEGURA...");
+        Terminal.cmd("CARGANDO MÓDULOS DE CIFRADO....................OK");
+        System.out.println();
+
         ServerSocket servidor = new ServerSocket(PUERTO);
-        System.out.println("Esperando al cliente...");
+        Terminal.so("ESPERANDO CONEXIÓN EN EL PUERTO " +  PUERTO +"................\nTERMINAL LISTA");
 
         Socket cliente = servidor.accept();
-        System.out.println("¡Cliente " + cliente.getInetAddress() + " conectado!");
-
+        Terminal.log("CLIENTE CONECTADO " + cliente.getInetAddress());
 
         GestorFirma firma = new GestorFirma();
-        System.out.println("Se están generando las claves pública y privada...");
-        System.out.println("Espere...");
-        System.out.println("¡Claves generadas con éxito!");
+        Terminal.log("GENERANDO PAR DE CLAVES RSA..................OK");
+        Terminal.log("CLAVE PÚBLICA TRANSMITIDA....................OK");
 
         PrintWriter out = new PrintWriter(cliente.getOutputStream(), true);
-
         String clavePublica = Base64.getEncoder().encodeToString(firma.getClavePublica().getEncoded());
         out.println(clavePublica);
-        System.out.println("¡Clave pública enviada al cliente!.");
         out.flush();
 
 
         // Pedimos el mensaje que se quiera enviar firmado
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduzca el mensaje a firmar:  ");
+        Terminal.separador();
+        Terminal.cmd("INTRODUCE EL MENSAJE A FIRMAR: ");
         String mensaje = scanner.nextLine();
 
         // El mensaje se firma
         byte[] firmaBytes = firma.getFirma(mensaje);
         String firma64 = Base64.getEncoder().encodeToString(firmaBytes);
-        System.out.println("Se ha firmado el mensaje.");
+        Terminal.log("MENSAJE FIRMADO..............................OK");
+        Terminal.log("MENSAJE TRANSMITIDO..........................OK");
+        Terminal.log("FIRMA TRANSMITIDA............................OK");
+
 
         out.println(mensaje);
-        System.out.println("Mensaje enviado al cliente.");
-
         out.println(firma64);
-        System.out.println("Firma enviada al cliente.");
+
+        Terminal.separador();
+        Terminal.cmd("CERRANDO CONEXIÓN...............................OK");
 
         scanner.close();
         cliente.close();
         servidor.close();
-        System.out.println("Conexión cerrada.");
-
     }
 }
---
+++ src/main/java/util/Terminal.java
@@ -0,0 +1,35 @@
+package util;
+
+public class Terminal {
+
+    public static void print(String txt) throws InterruptedException {
+        for (char ch : txt.toCharArray()) {
+            System.out.print(ch);
+            Thread.sleep(18);
+        }
+        System.out.println();
+    }
+    public static void separador() throws InterruptedException {
+        print("====================================================");
+    }
+    public static void log (String txt) throws InterruptedException {
+        print("| >> " + txt);
+    }
+    public static void cmd(String txt) throws InterruptedException {
+        print("> " + txt);
+    }
+    public static void cabecera(String nombreServer) throws InterruptedException {
+        separador();
+        print("   -SISTEMA OPERATIVO UNIFICADO INDUSTRIAS ROBCO-  ");
+        print("        COPYRIGHT 2075-2077 INDUSTRIAS ROBCO       ");
+        print("                   -" + nombreServer + "-             ");
+        separador();
+    }
+    public static void so(String txt) throws InterruptedException {
+        print("SO RobCo v.85");
+        print("(C) 2076 RobCo");
+        separador();
+        print(txt);
+        separador();
+    }
+}
